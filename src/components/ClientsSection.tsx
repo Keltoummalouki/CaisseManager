e5d7f706f893c0b123cda0ac735351358c174a4e
Keltoum Malouki <keltoummalouki911@gmail.com>
2025-06-19 18:00:25 +0100

    fix style

--- src/components/ClientsSection.tsx
+++ src/components/ClientsSection.tsx
@@ -5,13 +5,27 @@ import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import Image from 'next/image'
 
-const logos = [
-  '/clients/crusty.png',
-  '/clients/panda.png',
-  '/clients/lma3loma.png',
-  '/clients/theview360.png',
-  '/clients/fried.png',
+const restaurants = [
+  {
+    image: '/clients/Crusty.png',
+    nom: 'Crusty',
+    ville: 'Rabat',
+    adresse: 'Hay Riad Avenue Annakhil',
+  },
+  {
+    image: '/clients/Panda.png',
+    nom: 'Panda',
+    ville: 'Rabat',
+    adresse: 'Hay Riad Avenue Annakhil',
+  },
+  {
+    image: '/clients/lma3loma.png',
+    nom: 'Lma3loma',
+    ville: 'Casa',
+    adresse: 'Maarif, Bd Zerktouni',
+  }
 ]
+
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -63,25 +77,39 @@ export default function ClientsSection() {
       ref={sectionRef}
       className="relative h-screen bg-white dark:bg-black overflow-hidden"
     >
-      <h2 className="text-4xl font-bold text-center text-black dark:text-white py-10">
+      <h2 className="text-6xl font-bold text-center text-black dark:text-white py-10 mb-20">
           Nos Clients
       </h2>
       <div ref={trackRef}>
         <div className="flex w-max h-[70vh] items-center gap-10 px-40">
-          {logos.map((logo, index) => (
-            <div
-              key={index}
-              className="client-card min-w-[320px] h-[500px] bg-[#111] rounded-3xl px-10 py-14 shadow-xl flex items-center justify-center text-white text-xl font-semibold transition-all duration-500"
-            >
-              <Image
-                src={logo}
-                alt={`Logo ${index}`}
-                width={320}
-                height={500}
-                className="object-contain grayscale hover:grayscale-0 transition"
-              />
+        {restaurants.map((restau, index) => (
+          <div
+            key={index}
+            className="client-card min-w-[450px] h-[600px] bg-[#111] rounded-3xl px-12 py-16 shadow-2xl flex flex-col items-center justify-between text-white text-xl font-semibold transition-all duration-500"
+          > 
+            <Image
+              src={restau.image}
+              alt={`Logo ${index}`}
+              width={320}
+              height={500}
+              className="object-contain grayscale hover:grayscale-0 transition"
+            />
+            <div className="client-info mt-25 text-center ">
+              <h3 className="text-2xl font-black text-white mb-2 tracking-tight leading-tight">
+                {restau.nom}
+              </h3>
+              <p className="text-lg text-gray-300 font-medium mb-1">
+                {restau.ville}{restau.adresse}
+              </p>
+              <p className="text-sm text-gray-400 font-light italic leading-snug">
+
+              </p>
             </div>
-          ))}
+
+
+          </div>
+        ))}
+
 
           <div className="client-card cta-card min-w-[320px] h-[500px] bg-[#111] rounded-3xl px-10 py-14 shadow-xl flex items-center justify-center text-white text-xl font-semibold transition-all duration-500 hover:bg-purple-500 group">
             <a
